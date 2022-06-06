4325f72deae2848757e65f7fa75cceaaa82bd8f5
Andrei Dubov <av.dubov@yandex.ru>
2022-06-06 11:31:50 +0400

    initial commit

--- cache.go
+++ cache.go
@@ -2,22 +2,47 @@ package cache
 
 import "time"
 
+type data struct {
+	value     string
+	expiredAt time.Time
+}
+
 type Cache struct {
+	store map[string]data
 }
 
 func NewCache() Cache {
-	return Cache{}
+	return Cache{
+		store: make(map[string]data),
+	}
 }
 
-func (receiver) Get(key string) (string, bool) {
-
+func (c Cache) Get(key string) (string, bool) {
+	data, ok := c.store[key]
+	if !ok || time.Now().After(data.expiredAt) {
+		return "", false
+	}
+	return data.value, ok
 }
 
-func (receiver) Put(key, value string) {
+func (c *Cache) Put(key, value string) {
+	c.store[key] = data{value: value, expiredAt: time.Unix(1<<63-1, 0)}
 }
 
-func (receiver) Keys() []string {
+func (c Cache) Keys() []string {
+	keys := make([]string, 0)
+	for key, data := range c.store {
+		if !time.Now().After(data.expiredAt) {
+			keys = append(keys, key)
+		}
+	}
+	return keys
 }
 
-func (receiver) PutTill(key, value string, deadline time.Time) {
+func (c Cache) PutTill(key, value string, deadline time.Time) {
+	c.store[key] = data{
+		value:     value,
+		expiredAt: deadline,
+	}
+	return
 }
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/developer2912/golang-united-school-homework-5.2
+
+go 1.18
